451f3a851378436866c2dab2062e037adf96dc2d
Francesco Renzi <rentziass@gmail.com>
2017-05-10 12:10:53 +0200

    Update kowabunga.sh

--- kowabunga.sh
+++ kowabunga.sh
@@ -1,1 +1,52 @@
-sudo apt install git chromium-browser
+sudo apt install -y git chromium-browser
+
+### DOTFILES
+cd
+git clone https://github.com/rentziass/dotfiles.git dotfiles
+wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
+echo "deb http://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+sudo apt update
+sudo apt install rcm
+env RCRC=$HOME/dotfiles/rcrc rcup
+
+### VIM (with lua support)
+# Remove previous installations
+sudo apt-get remove vim vim-runtime vim-tiny vim-common
+
+# Install dependencies
+sudo apt-get install -y libncurses5-dev python-dev libperl-dev ruby-dev liblua5.2-dev
+
+# Fix liblua paths
+sudo ln -s /usr/include/lua5.2 /usr/include/lua
+sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.2.so /usr/local/lib/liblua.so
+
+# Clone vim sources
+cd ~
+git clone https://github.com/vim/vim.git
+
+cd vim
+./configure --prefix=/usr     \
+    --enable-luainterp=yes    \
+    --enable-perlinterp=yes   \
+    --enable-pythoninterp=yes \
+    --enable-rubyinterp=yes   \
+    --enable-cscope           \
+    --disable-netbeans        \
+    --enable-multibyte        \
+    --enable-largefile        \
+    --enable-gui=no           \
+    --with-features=huge      \
+    --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu
+
+make VIMRUNTIMEDIR=/usr/share/vim/vim74
+
+sudo apt-get install checkinstall
+sudo checkinstall
+
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+### ZSH
+sudo apt install -y zsh
+chsh -s $(which zsh)
